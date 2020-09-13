5409d9641bf7b1f582fa32d78ff7007ab88d4e30
mk0207 <matumototenpa@gmail.com>
2020-09-13 16:30:43 +0900

    バリデーション・エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   
   has_many :items
   has_many :buy_manage
+
+  validates :nickname, :birthday, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /@.+/}
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters'} do
+    validates :firstname
+    validates :lastname
+  end
+
+  validates :f_name_read, :l_name_read, presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
 end
