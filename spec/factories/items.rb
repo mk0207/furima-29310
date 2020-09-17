764b24e05bd1593a0c73c027c8c2ae3982131b5f
mk0207 <matumototenpa@gmail.com>
2020-09-17 17:43:03 +0900

    テストコード記述、実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :item do
+
+    # image {}
+    name {'商品名'}
+    description {'説明文'}
+    category_id {2}
+    status_id {2}
+    delfee_id {2}
+    ship_ori_id {2}
+    ship_day_id {2}
+    price {800}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('public/images/test_image.png')
+    end
+
+    it '全てに値が存在すれば登録できること' do
+      expect(@item).to be_valid
+    end
+
+    it 'imageが空では登録できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'nameが空では登録できないこと' do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'descriptionが空では登録できないこと' do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'category_idが選択されていないと登録できないこと' do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
+    it 'status_idが選択されていないと登録できないこと' do
+      @item.status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status Select")
+    end
+
+    it 'delfee_idが選択されていないと登録できないこと' do
+      @item.delfee_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delfee Select")
+    end
+
+    it 'ship_ori_idが選択されていないと登録できないこと' do
+      @item.ship_ori_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship ori Select")
+    end
+
+    it 'ship_day_idが選択されていないと登録できないこと' do
+      @item.ship_day_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship day Select")
+    end
+
+    it 'priceが空では登録できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+
+  end
 end
