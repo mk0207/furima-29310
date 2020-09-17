c512ecca8f547eb6ded6f4ec6f29f2bd95f9e604
mk0207 <matumototenpa@gmail.com>
2020-09-17 14:16:18 +0900

    エラーメッセージ、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,15 @@ class Item < ApplicationRecord
   has_one :buy_manage
   has_one_attached :image
 
-  validates :name, :description, :price, presence: true
+  validates :image, :name, :description, :price, presence: true
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :delfee_id, numericality: { other_than: 1 }
-  validates :ship_ori_id, numericality: { other_than: 1 }
-  validates :ship_day_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1, message:'Select' }
+  validates :status_id, numericality: { other_than: 1, message:'Select' }
+  validates :delfee_id, numericality: { other_than: 1, message:'Select' }
+  validates :ship_ori_id, numericality: { other_than: 1, message:'Select' }
+  validates :ship_day_id, numericality: { other_than: 1, message:'Select' }
+
+  validates :price, format: { with: /\A[0-9]+\z/ , message: 'Half-width number' }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: 'Out of setting range' }
 
 end
